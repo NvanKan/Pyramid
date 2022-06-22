017ace0731ffc5b827fcfd1578a91596e2af79f3
Niko <nikovk25@gmail.com>
2022-06-21 21:34:29 -0400

    xml Work

--- Spring1xml/src/main/java/org/example/App.java
+++ Spring1xml/src/main/java/org/example/App.java
@@ -1,13 +1,16 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App{
+
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+
+
+
     }
 }
--- Spring1xml/src/test/java/org/example/AppTest.java
+++ Spring1xml/src/test/java/org/example/AppTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,6 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+
 }
